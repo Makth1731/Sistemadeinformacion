b1edee763cf4aa329e89c993eaf4948e092b324d
julia <julia@DESKTOP-DS46TQ5>
2021-11-07 19:20:39 -0500

    que les vanga xd

--- SecuritySoft/src/co/edu/unbosque/model/Jugador.java
+++ SecuritySoft/src/co/edu/unbosque/model/Jugador.java
@@ -1,5 +1,37 @@
 package co.edu.unbosque.model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import co.edu.unbosque.model.persistence.Ljugador;
+
+
 public class Jugador {
+	
+	File Archivo;
+	
+	public void crearArchivoDatosJ() {
+		try {
+			Archivo = new File ("Datos de los Jugadores" );
+			if (Archivo.createNewFile()) {
+				JOptionPane.showMessageDialog(null, "Archivo Creado");
+				
+			}
+		}catch(IOException e) {
+			System.out.println(e);
+			
+		}
+	}
+	public void EscribirArchivo (Ljugador Ljugador) {
+		try {
+			FileWriter Escribir = new FileWriter (Archivo, true);
+			Escribir.write(Ljugador.getNombre() + "%" + Ljugador.getEdad() + "%" + Ljugador.getGenero() ); 
+		    Escribir.close();
+		    
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+	}
 
 }
--- SecuritySoft/src/co/edu/unbosque/model/persistence/Ljugador.java
+++ SecuritySoft/src/co/edu/unbosque/model/persistence/Ljugador.java
@@ -1,5 +1,42 @@
 package co.edu.unbosque.model.persistence;
 
 public class Ljugador {
+	
+	private String Nombre;
+	private String Edad;
+	private String Genero;
+	
+	public void Jugador (String Nombre, String Genero, String edad) {
+		
+		this.Nombre = Nombre;
+		this.Edad = Edad;
+		this.Genero = Genero;
+		
+		
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getEdad() {
+		return Edad;
+	}
+
+	public void setEdad(int edad) {
+		Edad = Edad;
+	}
+
+	public String getGenero() {
+		return Genero;
+	}
+
+	public void setGenero(String genero) {
+		Genero = genero;
+	}
 
 }
