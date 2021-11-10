6d1eb9a238c8c1ef5511af4f73cfd3bbcc826b9b
sago1 <sago1@LAPTOP-EC6SM6UU>
2021-11-10 11:25:44 -0500

    Vista listo

--- SecuritySoft/src/co/edu/unbosque/controller/Controller.java
+++ SecuritySoft/src/co/edu/unbosque/controller/Controller.java
@@ -7,6 +7,8 @@ import co.edu.unbosque.view.Msg;
 import co.edu.unbosque.view.PanelA;
 import co.edu.unbosque.view.PanelB;
 import co.edu.unbosque.view.PanelC;
+import co.edu.unbosque.view.PanelD;
+import co.edu.unbosque.view.vistajuegos;
 import co.edu.unbosque.view.vistajugador;
 import co.edu.unbosque.view.vistapartida;
 import co.edu.unbosque.view.vistaprincipal;
@@ -14,24 +16,29 @@ import co.edu.unbosque.view.vistaprincipal;
 public class Controller implements ActionListener {
 
 	private vistaprincipal vp;
-	private vistajugador vj;
+	private vistajugador vjg;
 	private vistapartida vpp;
+	private vistajuegos vj;
 	private PanelA pa;
 	private PanelB pb;
 	private PanelC pc;
+	private PanelD pd;
 	private Msg msg;
 	
 	public Controller() {
 		vp = new vistaprincipal();
-		vj = new vistajugador();
+		vjg = new vistajugador();
 		vpp = new vistapartida();
+		vj = new vistajuegos();
 		pa = new PanelA();
 		pb = new PanelB();
 		pc = new PanelC();
+		pd = new PanelD();
 		msg = new Msg();
 		vp.setVisible(true);
-		vj.setVisible(true);
-		vpp.setVisible(true);
+		vjg.setVisible(false);
+		vj.setVisible(false);
+		vpp.setVisible(false);
 		asignarOyentes();
 	}
 	public void asignarOyentes() {
@@ -39,11 +46,35 @@ public class Controller implements ActionListener {
 		vp.getPbotones().getJugador().addActionListener(this);
 		vp.getPbotones().getPartida().addActionListener(this);
 		vp.getPbotones().getTorneos().addActionListener(this);
+		vjg.getPb().getBatras().addActionListener(this);
+		vpp.getPc().getBatras().addActionListener(this);
+		vj.getPd().getBatras().addActionListener(this);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		String comando = e.getActionCommand();
+    	if (comando.equals("Partida")) {
+    			vp.setVisible(false);
+				vpp.setVisible(true);
+			}
+    		else if(comando.equals("ATRAS")) {
+    			vp.setVisible(true);
+				vpp.setVisible(false);
+				vj.setVisible(false);
+				vjg.setVisible(false);
+				
+			}
+    		else if(comando.equals("Jugador")) {
+    			vp.setVisible(false);
+				vjg.setVisible(true);
+				
+			}
+    		else if(comando.equals("Juegos")) {
+    			vp.setVisible(false);
+				vj.setVisible(true);
+				
+			}
 		
 	}
 	
