b72a0013c50e6d8e575138dd4c5679b2f128406e
USUARIO <USUARIO@DESKTOP-I7SR1V9>
2021-11-10 04:37:27 -0500

    Sujeto a modificaciones

--- SecuritySoft/src/co/edu/unbosque/model/Partida.java
+++ SecuritySoft/src/co/edu/unbosque/model/Partida.java
@@ -1,5 +1,37 @@
 package co.edu.unbosque.model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import co.edu.unbosque.model.persistence.Lpartida;
+
+
 public class Partida {
+	
+	File Archivo;
+	
+	public void crearArchivoDatosP() {
+		try {
+			Archivo = new File ("Datos de las partidas" );
+			if (Archivo.createNewFile()) {
+				JOptionPane.showMessageDialog(null, "Archivo Creado");
+				
+			}
+		}catch(IOException e) {
+			System.out.println(e);
+			
+		}
+	}
+	public void EscribirArchivo (Lpartida Lpartida) {
+		try {
+			FileWriter Escribir = new FileWriter (Archivo, true);
+			Escribir.write(Lpartida.getTipoPar() + "%" + Lpartida.getJugador1() + "%" + Lpartida.getJugador2() + "%" + Lpartida.getPuntaje1() + "%" + Lpartida.getPuntaje2() ); 
+		    Escribir.close();
+		    
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+	}
 
 }
--- SecuritySoft/src/co/edu/unbosque/model/persistence/Lpartida.java
+++ SecuritySoft/src/co/edu/unbosque/model/persistence/Lpartida.java
@@ -1,5 +1,74 @@
 package co.edu.unbosque.model.persistence;
 
 public class Lpartida {
+	
+	private String TipoPar;
+	private String Jugador1;
+	private String Jugador2;
+	private double Puntaje1;
+	private double Puntaje2;
+	
+	
+	public void Jugador (String TipoPar, String Jugador1, String Jugador2, double Puntaje1, double Puntaje2) {
+		
+		this.TipoPar = TipoPar;
+		this.Jugador1 = Jugador1;
+		this.Jugador2 = Jugador2;
+		this.Puntaje1 = Puntaje1;
+		this.Puntaje2 = Puntaje2;
+		
+		
+	}
 
+
+	public String getTipoPar() {
+		return TipoPar;
+	}
+
+
+	public void setTipoPar(String tipoPar) {
+		TipoPar = tipoPar;
+	}
+
+
+	public String getJugador1() {
+		return Jugador1;
+	}
+
+
+	public void setJugador1(String jugador1) {
+		Jugador1 = jugador1;
+	}
+
+
+	public String getJugador2() {
+		return Jugador2;
+	}
+
+
+	public void setJugador2(String jugador2) {
+		Jugador2 = jugador2;
+	}
+
+
+	public double getPuntaje1() {
+		return Puntaje1;
+	}
+
+
+	public void setPuntaje1(double puntaje1) {
+		Puntaje1 = puntaje1;
+	}
+
+
+	public double getPuntaje2() {
+		return Puntaje2;
+	}
+
+
+	public void setPuntaje2(double puntaje2) {
+		Puntaje2 = puntaje2;
+	}
+
+	
 }
