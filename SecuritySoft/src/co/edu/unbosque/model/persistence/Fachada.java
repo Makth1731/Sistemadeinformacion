bd6cf36eaabaf75cf0b4129a43545f2a21fa2d7b
sago1 <sago1@LAPTOP-EC6SM6UU>
2021-11-17 23:53:15 -0500

    Facha rey

---
+++ SecuritySoft/src/co/edu/unbosque/model/persistence/Fachada.java
@@ -0,0 +1,31 @@
+package co.edu.unbosque.model.persistence;
+
+public class Fachada {
+
+	Ljuego ljg = new Ljuego();
+	Lpartida lp= new Lpartida();
+	Ljugador lj = new Ljugador();
+	
+	public String printLjuego() {
+		return ljg.getJuego1()+
+		 ljg.getJuego2()+
+		 ljg.getJuego3();
+	}
+
+	public String printLpartida() {
+		return lp.getTipoPar()+
+		 lp.getJugador1()+
+		 lp.getJugador2()+
+		lp.getPuntaje2()+
+		lp.getPuntaje2();
+	}
+	public String printLjugador() {
+		return 
+		 lj.getNombre1()+
+		 lj.getNombre2()+
+		lj.getEdad1()+
+		lj.getGenero1()+
+		 lj.getGenero2()+
+		lj.getEdad2();
+	}
+}
