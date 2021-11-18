c3f26ea0868f608b1147669b554b477fad12fe02
sago1 <sago1@LAPTOP-EC6SM6UU>
2021-11-17 23:59:35 -0500

    CONTY

--- SecuritySoft/src/co/edu/unbosque/controller/Controller.java
+++ SecuritySoft/src/co/edu/unbosque/controller/Controller.java
@@ -2,14 +2,17 @@ package co.edu.unbosque.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import co.edu.unbosque.model.persistence.Fachada;
 import co.edu.unbosque.view.Vista;
 
 public class Controller implements ActionListener{
 	
 	private Vista gui;
-
+	private Fachada f;
+	
 	public Controller() {
-		
+		f = new Fachada();
 		gui = new Vista(this);
 		gui.setVisible(true);
 	}
