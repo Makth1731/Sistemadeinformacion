bf543b2e89eba036e9d406c82403e2cd329498e7
sago1 <sago1@LAPTOP-EC6SM6UU>
2021-11-29 01:49:53 -0500

    Borron y cuenta nueva

--- SecuritySoft/src/co/edu/unbosque/controller/Controller.java
+++ SecuritySoft/src/co/edu/unbosque/controller/Controller.java
@@ -1,26 +1,5 @@
 package co.edu.unbosque.controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+public class Controller {
 
-import co.edu.unbosque.model.persistence.Fachada;
-import co.edu.unbosque.view.Vista;
-
-public class Controller implements ActionListener{
-	
-	private Vista gui;
-	private Fachada f;
-	
-	public Controller() {
-		f = new Fachada();
-		gui = new Vista(this);
-		gui.setVisible(true);
-	}
-
-	public void actionPerformed(ActionEvent evento) {
-		
-		gui.getPanelResultado();
-	
-	}
-	
 }
